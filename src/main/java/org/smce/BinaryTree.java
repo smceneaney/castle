0aba70c5820304fd0f2e74871ef88630dc00f927
Shane McEneaney <smceneaney@gmail.com>
2020-06-30 14:04:50 +0100

    my second commit

--- src/main/java/org/smce/BinaryTree.java
+++ src/main/java/org/smce/BinaryTree.java
@@ -7,9 +7,16 @@ public class BinaryTree {
             return null;
         }
         Node parentNode = new Node(ints[0], null, null);
+        Node topNode = parentNode;
         for (int i = 1; i < ints.length; i++) {
-
+            Node childNode = new Node(ints[i], null, null);
+            if (childNode.getNodeValue() < parentNode.getNodeValue()) {
+                parentNode.setLeftNode(childNode);
+            } else {
+                parentNode.setRightNode(childNode);
+            }
+            parentNode = childNode;
         }
-        return null;
+        return topNode;
     }
 }
